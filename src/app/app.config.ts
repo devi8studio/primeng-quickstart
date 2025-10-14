3cdc0591e14d8f0f768e9f0d7901e032a226c024
Cagatay Civici <cagatay.civici@gmail.com>
2025-10-14 13:03:34 +0300

    update theme

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,14 +3,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import Aura from '@primeuix/themes/aura';
+import AcmeTheme from './acme-theme';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Aura,
+        preset: AcmeTheme,
         options: { darkModeSelector: '.p-dark' },
       },
     }),
